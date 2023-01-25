a700805059c78772e822ae35bf9660633fdb6f1e
Christopher <63862340+10Electra@users.noreply.github.com>
2023-01-25 10:59:36 +0000

    Un-ignore and add new OPD main script

---
+++ main.m
@@ -0,0 +1,9 @@
+filePath = 'C:\Users\cqh27498\Downloads\Atomic Precision Mount Tests\';
+fileName = '18um Al 5x mag, 0.55FOV.OPD';
+fullPath = strcat(filePath,fileName);
+[fid,array,wavelength,aspect,pxlsize] = ReadOPD(fullPath);
+clear filePath fileName fullPath
+surf(array,EdgeColor="none")
+daspect([1,1,1])
+ylim([350 550])
+xlim([0 1200])
