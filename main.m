c9cf0b9f1b89a47d77a2855fc26bc1f93a22952d
Christopher <63862340+10Electra@users.noreply.github.com>
2023-01-31 10:43:53 +0000

    Make main.m more readable

--- main.m
+++ main.m
@@ -1,21 +1,30 @@
+%% Have to supply the script with this information
 filePath = 'C:\Users\cqh27498\Downloads\Atomic Precision Mount Tests\';
-% fileName = '18um Al 5x mag, 0.55FOV.OPD';
-fileName = '25um Al, 300um wide.OPD';
+fileName = '18um Al 5x mag, 0.55FOV.OPD';
+
+% Bounds of the foil in terms of data columns
 left_foil_bound = 140;
 right_foil_bound = 1090;
+
+% Defines the search width
 approx_beam_width_um = 10;
+
+% Put the original data's vertical bounds here in μm for absolute heights.
+% Comment these out if absolute heights aren't needed
 realMinZ = -6.744;
 realMaxZ = 10.302;
 
-%% 
+%% Reads the .OPD file and correctly scales it if necessary
 fullPath = strcat(filePath,fileName);
 [array,wavelength,aspect,pxlsize] = ReadOPD(fullPath);
-% array = (array / 13.027) + 8.222;
+
+ % Comment this out if absolute heights not needed
 array = RecalibrateZ(array, realMinZ, realMaxZ);
+
 clear filePath fileName fullPath realMinZ realMaxZ
 
-%% 
-approx_beam_width_px = round(approx_beam_width_um/(pxlsize*1000)); % Need to work out scaling.
+%% Calculate total search bounds and search width
+approx_beam_width_px = round(approx_beam_width_um/(pxlsize*1000));
 sprintf('Search width is %d data point columns',approx_beam_width_px)
 
 foil_centre = (left_foil_bound + right_foil_bound) / 2;
@@ -23,7 +32,9 @@ left_third_bound = round(1/3 * (right_foil_bound-left_foil_bound) + left_foil_bo
 right_third_bound = round(2/3 * (right_foil_bound-left_foil_bound) + left_foil_bound);
 total_search_width_px = right_third_bound-left_third_bound;
 
-%%
+%% Goes through data column slices and plots them
+% Useful for viewing the data and testing new 'warp indicator' functions
+
 % for xi = left_third_bound:right_third_bound
 %     y = array(:,xi);
 %     x = linspace(1,height(array),height(array))';
@@ -31,19 +42,15 @@ total_search_width_px = right_third_bound-left_third_bound;
 %     xfit = rmmissing(x);
 %     yfit = rmmissing(y);
 %     scatter(xfit,yfit,'filled')
-% %     hold on
-% %     fitobj = fit(xfit,yfit,'poly2');
-% %   plot(fitobj)
-% %     fitobj2 = fit(xfit,yfit,'poly1');
-% %     plot(fitobj2)
 %     title(xi)
 %     waitforbuttonpress
 %     clf
 % end
 % clear x y xfit yfit xi
-%% 
-m = zeros(total_search_width_px,1);
-r = zeros(total_search_width_px,1);
+
+%% Calculates 'warp indicator' arrays for use later
+m = zeros(total_search_width_px,1); % Gradients of best fit lines
+r = zeros(total_search_width_px,1); % (Max - min) values
 for i=1:total_search_width_px
     j = i-1 + left_third_bound;
     fitobj = FitSlice(array,j,'poly1');
@@ -52,9 +59,10 @@ for i=1:total_search_width_px
     m(i) = fitobj.p1;
 end
 
+%% Finding the least warped section of the foil
 i = 0;
-lsf = inf;
-bsf = NaN;
+lowest = inf; % Lowest warp indicator average so far
+best = NaN; % best location to shoot the laser so far
 while i + approx_beam_width_px <= total_search_width_px
     j = i + left_third_bound;
     total = 0;
@@ -62,41 +70,27 @@ while i + approx_beam_width_px <= total_search_width_px
         total = total + r(i+k);
     end
     avg = total / approx_beam_width_px;
-    if lsf > avg
-        lsf = avg;
-        bsf = j+approx_beam_width_px/2; % 0.5 less than actual value
+    if lowest > avg
+        lowest = avg;
+        best = j+approx_beam_width_px/2; % 0.5 less than actual value
     end
     i=i+1;
 end
 clear i j k m r minCol maxCol total fitobj avg
 
-%% 
+%% Text outputs to command window
 sprintf(['The middle column of the %d column wide least warped section' ...
-    ' is %d'],[approx_beam_width_px,bsf])
-sprintf('The average gradient of this section is %d',lsf)
+    ' is %d'],[approx_beam_width_px,best])
+sprintf('The average gradient of this section is %d', ...
+    lowest*pxlsize)
+sprintf('The range of the middle column in this section is %d microns', ...
+    max(array(:,best))-min(array(:,best)))
 
-%%
+%% Plotting
 figure(1)
-plot3([bsf,bsf],[0,height(array)],[0,0],'r')
+plot3([best,best],[0,height(array)],[0,0],'r')
 hold on
 plot3([left_third_bound,left_third_bound],[0,height(array)],[0,0],'b')
 plot3([right_third_bound,right_third_bound],[0,height(array)],[0,0],'b')
 surf(array,EdgeColor="none")
 daspect([6,6,1])
-% daspect([1,1,1])
-ylim([350 600])
-% xlim([0 1200])
-hold off
-
-%%
-% figure(2)
-% plot(FitSlice(array,bsf,'poly1'))
-% hold on
-% y = array(:,bsf);
-% x = linspace(1,height(array),height(array))';
-% x(isnan(y)) = NaN;
-% xfit = rmmissing(x);
-% yfit = rmmissing(y);
-% scatter(xfit,yfit,'filled')
-% hold off
-% clear x y xfit yfit
