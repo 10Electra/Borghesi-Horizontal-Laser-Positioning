b9e5949cf3e66b2f27a2defcfaa0a88684698acb
Christopher <63862340+10Electra@users.noreply.github.com>
2023-02-28 11:31:44 +0000

    Improve plotting for analysis.m

--- analysis.m
+++ analysis.m
@@ -8,13 +8,20 @@ numIDs = str2double(strcat(n(:,1),n(:,2)));
 angles = targetDataTable.angle;
 R2s = targetDataTable.R2;
 
+sz = 50;
+c = idivide(int16(numIDs),int16(10));
 
-tiledlayout(1,2)
+tiledlayout(2,1)
 
-nexttile
-scatter(numIDs,angles)
+ax1 = nexttile;
+scatter(numIDs,angles,sz,c,'filled')
 ylabel('Correction angle [°]')
+xlabel('Target ID (e.g. 3B2 -> 32; 1D3 -> 13)')
+title('Correction Angle for all Characterised Targets','Grouped by Array');
 
-nexttile
-scatter(numIDs,R2s)
+ax2 = nexttile;
+scatter(numIDs,R2s, sz,c,'filled')
+ylim([0,1])
 ylabel('R^2 value')
+xlabel('Target ID (e.g. 3B2 -> 32; 1D3 -> 13)')
+title('R^2 Value for all Characterised Targets','Grouped by Array');
